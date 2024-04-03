e99524b548b2e0b0149ad6ab2c2cfca311b4a812
jhchoi316 <chlwogns316@gmail.com>
2024-04-03 20:52:10 +0900

    [2024.04.03]

---
+++ Baekjoon/06. 큐/10845.py
@@ -0,0 +1,32 @@
+import sys
+
+n = int(sys.stdin.readline())
+queue = []
+
+for i in range(n):
+    command = list(sys.stdin.readline().split())
+    
+    if command[0] == 'push':
+        queue.insert(0, command[1])
+    elif command[0] == 'pop':
+        if queue:
+            print(queue.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'front':
+        if queue:
+            print(queue[len(queue)-1])
+        else:
+            print(-1)
+    elif command[0] == 'back':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
