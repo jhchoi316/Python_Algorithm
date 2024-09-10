724c48979e2d8e03ad86600559bc562f525c3c47
jhchoi316 <chlwogns316@gmail.com>
2024-09-11 00:07:04 +0900

    [24.09.10]

---
+++ Baekjoon/15. 다이나믹 프로그래밍/11727.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+tiles = [0] * 1001
+
+tiles[0] = 1
+tiles[1] = 1
+
+if n >= 2:
+    for i in range(2, n):
+        tiles[i] = (tiles[i-2]*2) + tiles[i-1]
+
+print(tiles[n] % 10007)
