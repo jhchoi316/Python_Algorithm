377ae565bebf3fea26480f0b194c393b03d5c185
jhchoi316 <chlwogns316@gmail.com>
2024-10-30 16:43:59 +0900

    [24.10.30]

---
+++ Baekjoon/Silver V/1268(구현).py
@@ -0,0 +1,21 @@
+n = int(input())
+
+students = [[0]*n for i in range(n)]
+data = [list(map(int,input().split())) for i in range(n)]
+
+for i in range(n):
+    for j in range(5):
+        for k in range(n):
+            if data[i][j] == data[k][j]:
+                students[i][k] = 1
+
+answers = [0] * n
+
+for i, student in enumerate(students):
+    answers[i] = student.count(1)
+
+
+for i in range(n):
+    if answers[i] == max(answers):
+        print(i+1)
+        break
