1bf560cdffef013fde33cb2db0e1eeb80e7faf25
jhchoi316 <chlwogns316@gmail.com>
2025-02-20 16:16:41 +0900

    [25.02.20 ]

--- Baekjoon/Algorithm_Study/13251.py
+++ Baekjoon/Algorithm_Study/13251.py
@@ -1,16 +1,40 @@
 import sys
-import math
 read = sys.stdin.readline
 
-M = int(read())
-pebbles = list(map(int, read().split()))
+N = int(read())
+M = list(map(int, read().split()))
 K = int(read())
-N = sum(pebbles)
+possible = 0
 
-total = math.comb(N, K)
-same_colour = 0
+def factorial(n):
+    tmp = 1
+    for i in range(1, n+1):
+        tmp *= i
+    return tmp
 
-for p in pebbles:
-    same_colour += math.comb(p, K)
 
-print(same_colour/total)
+n = sum(M)
+total = factorial(n)//(factorial(n-K)*factorial(K))
+
+for i in M:
+    possible += factorial(i)//(factorial(i-K)*factorial(K))
+
+            
+print(possible/total)
+
+# import sys
+# import math
+# read = sys.stdin.readline
+
+# M = int(read())
+# pebbles = list(map(int, read().split()))
+# K = int(read())
+# N = sum(pebbles)
+
+# total = math.comb(N, K)
+# possible = 0
+
+# for p in pebbles:
+#     possible += math.comb(p, K)
+
+# print(possible/total)
